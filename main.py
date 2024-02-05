9afc903dcfac23f8b4dc9004320e6dee85733695
lifix1 <Nostarw1@yandex.ru>
2024-02-05 17:52:23 +0300

    first_task

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ import sys
 import os
 import pygame
 
-x = input()
-y = input()
-z = input()
-url = f'https://static-maps.yandex.ru/1.x/?ll={x},{y}&z={z}&l=map'
-response = requests.get(url)
+running = True
+params = {
+    'll': '28.333762,57.819387',
+    'z': '17',
+    'l': 'map'
+}
+url = f'https://static-maps.yandex.ru/1.x/'
+response = requests.get(url=url, params=params)
 
 if not response:
     print("Ошибка выполнения запроса:")
     print(url)
     print("Http статус:", response.status_code, "(", response.reason, ")")
     sys.exit(1)
-
 map_file = "map.png"
 with open(map_file, "wb") as file:
     file.write(response.content)
-
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
@@ -26,5 +27,5 @@ pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
     pass
 pygame.quit()
-
 os.remove(map_file)
+
