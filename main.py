8f7dd0ad33b399c64efa4fdc1241196b1c6745a7
lifix1 <Nostarw1@yandex.ru>
2024-02-05 18:21:34 +0300

    second_task

--- main.py
+++ main.py
@@ -34,13 +34,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.key == pygame.K_PAGEUP:
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEUP:
             params['z'] = params['z'] + 1
             if params['z'] > 21:
                 print('Больше нельзя отдалить')
                 params['z'] = 21
             find()
-        elif event.key == pygame.K_PAGEDOWN:
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEDOWN:
             params['z'] = params['z'] - 1
             if params['z'] < 0:
                 print('Больше нельзя приблизить')
