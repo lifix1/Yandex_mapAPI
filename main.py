733f630904ce97ba18dde513c1c588c5247dff37
lifix1 <Nostarw1@yandex.ru>
2024-02-05 18:20:20 +0300

    second_task

--- main.py
+++ main.py
@@ -6,26 +6,44 @@ import pygame
 running = True
 params = {
     'll': '28.333762,57.819387',
-    'z': '17',
+    'z': 17,
     'l': 'map'
 }
 url = f'https://static-maps.yandex.ru/1.x/'
-response = requests.get(url=url, params=params)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(url)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
 map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
-os.remove(map_file)
 
+
+def find():
+    response = requests.get(url=url, params=params)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(url)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+
+
+find()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.K_PAGEUP:
+            params['z'] = params['z'] + 1
+            if params['z'] > 21:
+                print('Больше нельзя отдалить')
+                params['z'] = 21
+            find()
+        elif event.type == pygame.K_PAGEDOWN:
+            params['z'] = params['z'] - 1
+            if params['z'] < 0:
+                print('Больше нельзя приблизить')
+                params['z'] = 0
+            find()
+pygame.quit()
